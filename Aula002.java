1d82359d17de743075298cd3a315bf46ee6d8f52
joao-victor-barbosa14 <joao.v.ferreira.barbosa@gmail>
2026-03-25 16:52:15 -0300

    Variaveis e constantes

---
+++ Aula002.java
@@ -0,0 +1,13 @@
+public class Aula002 {
+    
+    public static void main(String[] args) {
+        int number01 = 15;
+        int number02 = 15;
+        double pi = 3.14;
+        
+        int soma = number01 + number02;
+        
+        System.out.println("Soma: " + soma);
+        System.out.println("O valor de PI é:" + pi);
+    }
+}
