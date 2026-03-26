0e3048773eac383433740e7b9932c7d279128ae7
joao-victor-barbosa14 <joao.v.ferreira.barbosa@gmail>
2026-03-26 20:12:39 -0300

    Inferência

---
+++ Aula004.java
@@ -0,0 +1,15 @@
+public class Aula004 {
+    public static void main(String[] args) {
+
+        var a = "Hello word";
+        System.out.println(a);
+
+        var b = 20.10;
+        System.out.println(b);
+
+        var c = 2010;
+        System.out.println(c);
+
+     } }
+
+
