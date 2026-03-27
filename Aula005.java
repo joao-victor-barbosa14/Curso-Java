4384f4f0ab79fe8a700be39663b327f59ba068dd
joao-victor-barbosa14 <joao.v.ferreira.barbosa@gmail>
2026-03-27 15:04:40 -0300

    Tipos Primitivos

---
+++ Aula005.java
@@ -0,0 +1,31 @@
+public class Aula005 {
+    public static void main(String[] args) {
+        //Informações de usuário
+
+        //Tipos Numéricos inteiros
+        byte AnosDeEmpresa = 23;
+        short NumeroDeVoos = 545;
+        int id = 123456;
+        long PontosAcumulados = 13245679465123l;
+
+        //Tipos Numéricos reais
+        float Salário = 1200.00f;
+        double VendasAcumuladas = 1500.00;
+
+        //Tipo Booleano
+        boolean EstaDeFerias = false;
+
+        //Charactere
+        char status = 'A';
+
+        System.out.println("Anos de empresa: " + AnosDeEmpresa);
+        System.out.println("Número de voos: " + NumeroDeVoos);
+        System.out.println("ID do funcionário: " + id);
+        System.out.println("Pontos Acumulados: " + PontosAcumulados);
+        System.out.println("Salário: " + Salário);
+        System.out.println("Vendas acumuladas: " + VendasAcumuladas);
+        System.out.println("Funcionário está de Férias: " + EstaDeFerias);
+        System.out.println("Status: " + status);
+
+    }
+}
