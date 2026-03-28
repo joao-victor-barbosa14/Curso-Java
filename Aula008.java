26318a89a4475d1bf85a3047cd73140e0b1923f1
joao-victor-barbosa14 <joao.v.ferreira.barbosa@gmail>
2026-03-28 10:59:19 -0300

    Tipo String

---
+++ Aula008.java
@@ -0,0 +1,9 @@
+public class Aula008 {
+    public static void main(String[] args) {
+        var Carro = "Mustang";
+        var Ano = 2026;
+        var Preço = 126.000;
+
+        System.out.printf("Nome: %s Ano: %d Preço: %.2f", Carro, Ano, Preço);
+    }
+}
